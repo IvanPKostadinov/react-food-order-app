f2b3909a8a144806dd5bfa7b63707defeabf29df
IvanPKostadinov <ikostadinov@melontech.com>
2022-03-14 18:31:08 +0200

    used the Cart Context in HeaderCartButton.js

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 
 const HeaderCartButton = (props) => {
+  // Now this component will be re-evaluated by React whenever
+  // the Context changes, i.e. we update it in the CartProvider Component
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onShowCartClicked}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
