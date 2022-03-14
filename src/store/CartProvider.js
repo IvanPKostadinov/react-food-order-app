1a87c70b297d31efbd9077f48f49c2b1e65549c6
IvanPKostadinov <ikostadinov@melontech.com>
2022-03-14 23:32:29 +0200

    added useReducer() in CartProvider.js to manage the State of the cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,15 +1,45 @@
+import { useReducer } from 'react';
+
 import CartContext from './cart-context';
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD_CART_ITEM') {
+    // array.concat(item) adds item to an array and returns NEW array
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
 
 const CartProvider = (props) => {
   // Here we add all the logic for managing the Context,
   // so that no other Component needs to deal with that.
-  const addItemToCartHandler = (item) => {};
 
-  const removeItemFromCartHandler = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: 'ADD_CART_ITEM', item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: 'REMOVE_CART_ITEM', id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
