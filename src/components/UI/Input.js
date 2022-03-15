c103a1f29fd54304da86a21794e7ccf9b070cda2
IvanPKostadinov <ikostadinov@melontech.com>
2022-03-15 12:00:27 +0200

    added Add To Cart functionality and used refs and forward refs

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,10 +1,15 @@
-import classes from './Input.module.css'
+import React from 'react';
 
-const Input = props => {
-  return <div className={classes.input}>
-    <label htmlFor={props.input.id}>{props.label}</label>
-    <input {...props.input} />
-  </div>
-};
+import classes from './Input.module.css';
+
+const Input = React.forwardRef((props, ref) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      {/* Here we are forwarding (using React.forwardRef) the ref to the Component that uses Input: */}
+      <input ref={ref} {...props.input} />
+    </div>
+  );
+});
 
 export default Input;
