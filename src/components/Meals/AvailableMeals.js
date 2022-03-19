3cec12546497a6f826b0fb0944eb49118b2363c2
IvanPKostadinov <ikostadinov@melontech.com>
2022-03-19 16:44:26 +0200

    added error handling with httpError State

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,13 +7,19 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState(null); // useState(); is also possible here
 
   useEffect(() => {
     const fetchMeals = async () => {
       setIsLoading(true);
       const response = await fetch(
-        'https://react-food-order-app-ce29c-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
+        'https://react-food-order-app-ce29c-default-rtdb.europe-west1.firebasedatabase.app/meals.json1'
       ); // fetch() returns a promise, hence async + await
+
+      if (!response.ok) {
+        throw new Error('Something went wrong.');
+      }
+
       const responseData = await response.json(); // .json() returns a promise, hence await
 
       const loadedMeals = [];
@@ -31,13 +37,26 @@ const AvailableMeals = () => {
       setIsLoading(false);
       console.log(loadedMeals);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      // console.log(error);
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes['meals-loading']}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes['meals-error']}>
+        <p>{httpError}</p>
       </section>
     );
   }
