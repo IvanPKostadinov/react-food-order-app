0f75e8d501692bef78791925bebf7c51dd3198ab
IvanPKostadinov <ikostadinov@melontech.com>
2022-03-21 14:35:17 +0200

    added POST method to Cart.js to send the selected Items and the User data to Firebase

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -26,6 +26,16 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
+  const submitOrderHandler = (userData) => {
+    fetch('https://react-food-order-app-ce29c-default-rtdb.europe-west1.firebasedatabase.app/orders.json', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items,
+      })
+    })
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
@@ -61,7 +71,9 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckout && modalActions}
     </Modal>
   );
